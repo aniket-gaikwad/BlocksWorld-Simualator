c00ec9c9ad84ad0ebcf80def75609fe3bdbb31b0
aniket-gaikwad <aniket0123@gmail.com>
2015-10-27 19:11:19 -0400

    Class environment created

---
+++ BlocksWorlds/src/environment.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class environment {
+
+	private ArrayList<Block> listOfBlocks;
+	
+	void initiate(){
+		// Read user file and create environment
+		// Do validation before creating blocks.
+	}
+	
+	void move(String color1,String color2){
+		// Get Block of color1 and color2
+		// Validate before moving Blocks
+	}
+	
+}
